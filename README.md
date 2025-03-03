8d87ab9a9c5f834f6075753ebbf77f17c0606353
the coder <abdoulwariskonate@gmail.com>
2025-03-03 14:57:54 +0100

    init commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# 4CITE_BACKEND
-Repo pour le projet de test et d'intégration d'un projet de gestion d'hôtels(avec pipeline CI/CD ) ce contient uniquement le code du backend
---
+++ src/main/java/com/example/springboothotelhandling/SpringBootHotelHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothotelhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHotelHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHotelHandlingApplication.class, args);
+    }
+
+}
